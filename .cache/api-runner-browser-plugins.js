1558333b86f5ad3d8ced5601315cc66149744231
Berkan Dirim <berkandirim@gmail.com>
2018-10-04 12:26:57 +0200

    configured as gatsby site

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('/Users/dirimber/Sites/evr-ber/node_modules/gatsby-plugin-offline/gatsby-browser'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/Users/dirimber/Sites/evr-ber/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ .cache/sync-requires.js
@@ -0,0 +1,11 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/dirimber/Sites/evr-ber/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/dirimber/Sites/evr-ber/src/pages/404.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/dirimber/Sites/evr-ber/src/pages/index.js")),
+  "component---src-pages-page-2-js": preferDefault(require("/Users/dirimber/Sites/evr-ber/src/pages/page-2.js"))
+}
+
